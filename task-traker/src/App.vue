fbc906001c19adddc92691247111b7bcbab6765b
Giuseppa Prestigiacomo <gprestigiacomo190@gmail.com>
2021-10-03 20:26:07 +0200

    new changes

---
+++ task-traker/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+<div class="container">
+  <Header title="Task Tracker" />
+  <Tasks :tasks="tasks"/>
+</div>
+ </template>
+
+<script>
+ import Header from './components/Header.vue';
+ import Tasks from './components/Tasks.vue';
+ 
+export default {
+  name: "App",
+  components: {
+    Header,
+    Tasks
+    
+  },
+  data() {
+    return {
+      tasks: []
+    }
+  },
+  created() {
+    this.tasks = [
+     {
+       id:1, 
+       text: 'Swimming lesson',
+       day:'October 4th at 3:00 p.m',
+       reminder: true,
+     },
+     {
+       id:2, 
+       text: 'Doctor appointment',
+       day:'October 14th at 1:00 p.m',
+       reminder: true,
+     },
+     {
+       id:3, 
+       text: 'Meeting at office',
+       day:'October 6th at 9:00 a.m',
+       reminder: true,
+     },
+    ]
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.container {
+  border: 1px solid blue; 
+  width: 100vh; 
+  height:100vh; 
+}
+</style>
